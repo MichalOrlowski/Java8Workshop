ac27bba4c32abfc7f9f2b4b27a3ddcd2e91c2e1f
Michal Orlowski <michal.orlowski@jcommerce.pl>
2017-10-26 21:38:31 +0200

    Testowanie z Marcinem

--- src/main/java/pl/jcommerce/examples/date/DateApiExample.java
+++ src/main/java/pl/jcommerce/examples/date/DateApiExample.java
@@ -19,8 +19,6 @@ public class DateApiExample {
 
         System.out.println(LocalDate.of(12, 12, 12));
 
-        // Checking again kodilla testing
-
 		// System.currentTimeMillis()
 		/*Clock clock = Clock.systemDefaultZone();
 		clock.millis();
